1fe26e28ea5fbd6b06f77382246cabfe5857f901
Yauhen Artsiukhou <jsirex@gmail.com>
2020-05-11 22:42:11 +0300

    Update backup function

--- plugins/backup/backup.plugin.zsh
+++ plugins/backup/backup.plugin.zsh
@@ -1,9 +1,9 @@
 backup-my-pc() {
     TARGET_DIR="$1"
-    BACKUP_FILE_NAME="`hostname -s`-backup-`date +%F`.tar.bz2"
+    BACKUP_FILE_NAME="`hostname -s`-backup-`date +%F`.tar.bz2.gpg"
     FULL_BACKUP_FILE_NAME=$TARGET_DIR/$BACKUP_FILE_NAME
 
-    EXCLUDE_FILE=~/.oh-my-zsh/custom/plugins/backup/backup.list
+    EXCLUDE_FILE=~/proj/jsirex/oh-my-zsh-custom/plugins/backup/backup.exclude
 
     if [ -z "$TARGET_DIR" ]; then
         echo "Specify target backup directory"
@@ -19,7 +19,7 @@ backup-my-pc() {
     echo "Target directory: $TARGET_DIR"
     echo "Target file: $FULL_BACKUP_FILE_NAME"
 
-    sudo ionice tar --exclude-from=$(readlink -f $EXCLUDE_FILE) --exclude=$FULL_BACKUP_FILE_NAME -cvpjf $FULL_BACKUP_FILE_NAME --one-file-system /
+    sudo ionice tar --exclude-from=$(readlink -f $EXCLUDE_FILE) --exclude=$FULL_BACKUP_FILE_NAME -cvpj --one-file-system / | gpg -c -o $FULL_BACKUP_FILE_NAME
 
     echo "Ended at `date`"
 }
